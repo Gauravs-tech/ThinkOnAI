dfae9138a711fc2db6fad31fae58c90f748c7c91
Gaurav Shegekar <gauravrshegekar@gmail.com>
2026-04-03 21:55:56 +0530

    foolter logo path corrected

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-[#0e0e10] w-full py-12 flex justify-center items-center border-t border-zinc-800/20">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8  max-w-7xl mx-auto font-body text-xs text-zinc-500">
         <div className="flex flex-col justify-center items-center gap-4">
-          <div className="text-lg font-bold flex text-zinc-200 font-headline justify-center items-center gap-1"><img className='h-10 w-10' src="./public/logo.png" alt="" /> ThinkOnAI</div>
+          <div className="text-lg font-bold flex text-zinc-200 font-headline justify-center items-center gap-1"><img className='h-10 w-10' src="/images/logo.png" alt="" /> ThinkOnAI</div>
           <p className="max-w-xs leading-relaxed">The high-end editorial newsletter for the AI era. <br /> strategic, and direct.</p>
           <div className="text-zinc-500 mt-4">© 2024 Aura Intelligence. The Digital Curator.</div>
         </div>
