d150831136b23a5250581e181867ce521c13e373
Gaurav Shegekar <gauravrshegekar@gmail.com>
2026-04-03 21:47:11 +0530

    logo path updated

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     <nav className="fixed top-0 w-full z-50 bg-zinc-950/60 backdrop-blur-xl shadow-[0_8px_32px_0_rgba(61,90,254,0.08)]">
       <div className="flex justify-between items-center px-8 py-4 max-w-7xl mx-auto">
         <Link to="/" className="text-xl font-bold tracking-tighter text-zinc-100 flex items-center gap-1 font-headline">
-          <img className='h-10 w-10 p-0' src="./public/logo.png" alt="" />
+          <img className='h-10 w-10 p-0' src="/public/logo.png" alt="" />
           <span className="material-symbols-outlined p-0 text-[#3D5AFE]" style={{ fontVariationSettings: "'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 24" }}></span>
           ThinkOnAI
         </Link>
